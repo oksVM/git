ea8e2a43885b07b288909bef1ee6a2e56efc53fd
Oksana Moisiuk <oksana_moisiuk@epam.com>
2021-12-03 01:06:02 +0200

    My

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.println("branch");
         System.out.println("branch");
         System.out.println("branch");
+        System.out.println("branch");
 
     }
 }
