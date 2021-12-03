a291db818fcc496ba1f32dacfa0227fc7d417944
Oksana Moisiuk <oksana_moisiuk@epam.com>
2021-12-03 02:06:59 +0200

    My

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,24 +4,5 @@ public class Main {
         System.out.printf("main");
         System.out.println("main");
         System.out.println("main");
-
-        System.out.println("branch");
-<<<<<<< HEAD
-<<<<<<< HEAD
-        System.out.println("branch");
-        System.out.println("branch");
-        System.out.println("branch");
-
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
-=======
->>>>>>> 5e904ac... My
-=======
-        System.out.println("branch");
-        System.out.println("branch");
-        System.out.println("branch");
-
->>>>>>> 1a9eefc... My
     }
 }
